35a41b57cbe62d3dec608623f687bf8c0bdb7e94
t <tysonpederson95@gmail.com>
2022-08-15 10:11:18 -0500

    Create index and js files, set up pseudocode for js file

---
+++ rock-paper-scissors.js
@@ -0,0 +1,22 @@
+function getComputerChoice() {
+    // return 'rock' 'paper' or 'scissors' randomly to computerSelection
+    
+    // generate randomNumber 0, 1 or 2
+    // if (randomNumber = 0), computerSelection = 'rock'
+    // if (randomNumber = 1), computerSelection = 'paper'
+    // if (randomNumber = 2), computerSelection = 'scissors'
+    // return computerSelection
+}
+
+function playRound(playerSelection, computerSelection) {
+    // should take 2 parameters, playerSelection and computerSelection
+    // return a string to declare winner like so "You Lose! Paper beats rock"
+        // return result, do not console.log here
+    // Make playerSelection work case-insensitively (rock, Rock, RocK etc.)
+}
+
+function game() {
+    // call playRound to play a 5 round game that keeps score and reports a winner/loser at the end
+    // At this point console.log is used to display the results of each round and the winner at the end
+    // Use prompt() to get input from the user
+}
